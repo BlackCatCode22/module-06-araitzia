aa874da16c1542c78a158b8cb707a33bdfc2830f
araitzia <158853590+araitzia@users.noreply.github.com>
2024-11-03 19:54:04 -0800

    Add files via upload

--- inheritance.py
+++ inheritance.py
@@ -1,7 +1,7 @@
 class PartyAnimal:
-
+    x = 0
+    name = ""
     def __init__(self, nam):
-        self.x = 0
         self.name = nam
         print(self.name, "constructed")
 
@@ -9,21 +9,15 @@ class PartyAnimal:
         self.x = self.x + 1
         print(self.name, "party count", self.x)
 
-
 class FootballFan(PartyAnimal):
-
-    def __init__(self, nam):
-        super().__init__(nam)
-        self.points = 0
-
+    points = 0
     def touchdown(self):
         self.points = self.points + 7
         self.party()
         print(self.name, "points", self.points)
 
-
 s = PartyAnimal("Sally")
-s.party()
+s.party
 
 j = FootballFan("Jim")
 j.party()
